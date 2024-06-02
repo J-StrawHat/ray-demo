39ebcae5d65ba0b72879158f87ee8bdd213554ce
J-StrawHat <422148166@qq.com>
2024-06-02 16:53:36 +0800

    Add logging statements to client.cc

--- src/client/client.cc
+++ src/client/client.cc
@@ -12,18 +12,21 @@ int main(int argc, char **argv) {
     assert(r);
     // Silence warning.
     (void)r;
+    printf("Writing to server: {\"hello\":\"ray\"}\n");
 
     auto get_result = [&client](const std::string &key) {
         bool ok;
         std::string result;
         std::tie(ok, result) = client.Get("hello");
         assert(ok);
-        std::cout << "Client got result: " << result << std::endl;
+        std::cout << "Client got value from \"hello\" key: " << result << std::endl;
     };
 
     get_result("hello");
 
     client.Put("hello", "world");
+    assert(r);
+    printf("Writing to server: {\"hello\":\"world\"}\n");
 
     get_result("hello");
 
